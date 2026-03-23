e0aa70579f05ffed80d6e9ce585625d31c51df80
Pooja <p1219513@gmial.com>
2026-03-23 09:38:59 +0530

    Add everything

--- src/predict.py
+++ src/predict.py
@@ -10,29 +10,27 @@ from uncertainty import compute_uncertainty
 
 def predict():
 
-    # -------------------------
-    # 0. Ensure output folder
-    # -------------------------
+
+    # Ensure output folder
     os.makedirs("outputs", exist_ok=True)
 
 
-    # -------------------------
-    # 1. Load test data
-    # -------------------------
+    # ------------------------------------------- 1. Load test data-----------------------------------------
+
     df = load_data("data/arvyax_test_inputs_120.xlsx - Sheet1.csv")
 
-    # -------------------------
-    # 2. Load models + tools
-    # -------------------------
+    
+    #--------------------------------------------- 2. Load models + tools-----------------------------------
+    
     model_state = joblib.load("models/state_model.pkl")
     model_intensity = joblib.load("models/intensity_model.pkl")
     tfidf = joblib.load("models/tfidf.pkl")
     scaler = joblib.load("models/scaler.pkl")
     feature_columns = joblib.load("models/feature_columns.pkl")
 
-    # -------------------------
-    # 3. Preprocess test data
-    # -------------------------
+    
+    #--------------------------------------------- 3. Preprocess test data------------------------------------------
+
     X_text, X_meta, _, _, _ = preprocess(
         df,
         fit=False,
@@ -43,9 +41,8 @@ def predict():
 
     X = hstack([X_text, X_meta])
 
-    # -------------------------
-    # 4. Predictions
-    # -------------------------
+    #-------------------------------------------4. Predictions-------------------------------------------------
+
     state_preds = model_state.predict(X)
     intensity_preds = model_intensity.predict(X)
 
@@ -54,16 +51,16 @@ def predict():
 
     print("State preds:", len(state_preds))
     print("Intensity preds:", len(intensity_preds))
-    # -------------------------
-    # 5. Uncertainty
-    # -------------------------
+    
+    #-------------------------------------------------- 5. Uncertainty------------------------------------
+
     confidence, uncertain_flag = compute_uncertainty(
         model_state, X, intensity_preds
     )
 
-    # -------------------------
-    # 6. Decision Engine
-    # -------------------------
+    
+    #-------------------------------------------------- 6. Decision Engine----------------------------------
+    
     actions = []
     timings = []
 
@@ -84,8 +81,8 @@ def predict():
         actions.append(action)
         timings.append(timing)
 
-    # -------------------------
-    # 7. Save output
+    
+    #---------------------------------------------- 7. Save output-------------------------------------------
     # -------------------------
     output = pd.DataFrame({
         "id": df["id"],
